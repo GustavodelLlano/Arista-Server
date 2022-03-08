13a275aa83a51838638c2d95a07107c4459fd9ff
Judit Angulo <judith1518@gmail.com>
2022-03-08 17:44:03 +0100

    cors

--- config/index.js
+++ config/index.js
@@ -22,7 +22,6 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      credentials: true,
       origin: process.env.ORIGIN || "http://localhost:3000",
     })
   );
